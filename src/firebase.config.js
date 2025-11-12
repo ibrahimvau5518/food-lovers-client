ba65797018fcbb856c1a2130f07c44f122bb84b8
ibrahimvau5518 <jibonm676@gmail.com>
2025-11-12 19:46:15 +0600

    Install firebase & setup the firebase config file

---
+++ src/firebase.config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBfoBNjf9D2SBbEv7SCqICnOPdAzdpQaAA',
+  authDomain: 'food-lovers-client.firebaseapp.com',
+  projectId: 'food-lovers-client',
+  storageBucket: 'food-lovers-client.firebasestorage.app',
+  messagingSenderId: '933695062947',
+  appId: '1:933695062947:web:ee03221a2d792410471016',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
